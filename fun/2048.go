fce5afc80528d21dd51a9f7071f9526f324ab71e
York lin <superj80820@gmail.com>
2023-10-04 15:12:22 +0800

    update

--- fun/2048.go
+++ fun/2048.go
@@ -18,10 +18,20 @@ const (
 	RIGHT
 )
 
+var gameProcessor = map[Action]struct {
+	Move       func(input [][]int)
+	Merge      func(input [][]int)
+	AddRandNum func(input [][]int)
+}{
+	UP:    {Move: upMove, Merge: upMerge, AddRandNum: addRandNum},
+	DOWN:  {Move: downMove, Merge: downMerge, AddRandNum: addRandNum},
+	LEFT:  {Move: leftMove, Merge: leftMerge, AddRandNum: addRandNum},
+	RIGHT: {Move: rightMove, Merge: rightMerge, AddRandNum: addRandNum},
+}
+
 var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
-
 	ch := make(chan Action)
 
 	go func() {
@@ -42,10 +52,10 @@ func main() {
 	}()
 
 	// input := [][]int{
-	// 	{0, 0, 0, 2},
-	// 	{0, 2, 8, 2},
-	// 	{2, 32, 16, 2},
-	// 	{32, 64, 4, 8},
+	// 	{0, 4, 0, 0},
+	// 	{2, 0, 0, 0},
+	// 	{2, 0, 0, 0},
+	// 	{8, 8, 2, 2},
 	// }
 
 	input := make([][]int, 4)
@@ -84,126 +94,134 @@ func printBoard(input [][]int) {
 }
 
 func gameProcess(input [][]int, action Action) {
-	switch action {
-	case UP:
-		for col := 0; col < len(input[0]); col++ {
-			var swapCellsRow []int
-			for row := 0; row < len(input); row++ {
-				if input[row][col] == 0 {
+	gameProcessor[action].Move(input)
+	gameProcessor[action].Merge(input)
+	gameProcessor[action].Move(input)
+	gameProcessor[action].AddRandNum(input)
+}
+
+func upMove(input [][]int) {
+	for col := 0; col < len(input[0]); col++ {
+		var swapCellsRow []int
+		for row := 0; row < len(input); row++ {
+			if input[row][col] == 0 {
+				swapCellsRow = append(swapCellsRow, row)
+			} else {
+				if len(swapCellsRow) != 0 {
+					var swapRow int
+					swapRow, swapCellsRow = swapCellsRow[0], swapCellsRow[1:]
 					swapCellsRow = append(swapCellsRow, row)
-				} else {
-					if len(swapCellsRow) != 0 {
-						var swapRow int
-						swapRow, swapCellsRow = swapCellsRow[0], swapCellsRow[1:]
-						swapCellsRow = append(swapCellsRow, row)
-						input[swapRow][col], input[row][col] = input[row][col], input[swapRow][col]
-					}
+					input[swapRow][col], input[row][col] = input[row][col], input[swapRow][col]
 				}
-			}
-		}
-
-		for col := 0; col < len(input[0]); col++ {
-			pre := -1
-			for row := 0; row < len(input); row++ {
-				if pre == input[row][col] {
-					input[row-1][col] = input[row][col] + pre
-					input[row][col] = 0
-				} else if pre == 0 {
-					input[row][col], input[row-1][col] = input[row-1][col], input[row][col]
-				}
-				pre = input[row][col]
-			}
-		}
-	case DOWN:
-		for col := 0; col < len(input[0]); col++ {
-			var swapCellsRow []int
-			for row := len(input) - 1; row >= 0; row-- {
-				if input[row][col] == 0 {
-					swapCellsRow = append(swapCellsRow, row)
-				} else {
-					if len(swapCellsRow) != 0 {
-						var swapRow int
-						swapRow, swapCellsRow = swapCellsRow[0], swapCellsRow[1:]
-						swapCellsRow = append(swapCellsRow, row)
-						input[swapRow][col], input[row][col] = input[row][col], input[swapRow][col]
-					}
-				}
-			}
-		}
-
-		for col := 0; col < len(input[0]); col++ {
-			pre := -1
-			for row := len(input) - 1; row >= 0; row-- {
-				if pre == input[row][col] {
-					input[row+1][col] = input[row][col] + pre
-					input[row][col] = 0
-				} else if pre == 0 {
-					input[row][col], input[row+1][col] = input[row+1][col], input[row][col]
-				}
-				pre = input[row][col]
-			}
-		}
-	case LEFT:
-		for row := 0; row < len(input); row++ {
-			var swapCellsCol []int
-			for col := 0; col < len(input[0]); col++ {
-				if input[row][col] == 0 {
-					swapCellsCol = append(swapCellsCol, col)
-				} else {
-					if len(swapCellsCol) != 0 {
-						var swapCol int
-						swapCol, swapCellsCol = swapCellsCol[0], swapCellsCol[1:]
-						swapCellsCol = append(swapCellsCol, col)
-						input[row][swapCol], input[row][col] = input[row][col], input[row][swapCol]
-					}
-				}
-			}
-		}
-
-		for row := 0; row < len(input); row++ {
-			pre := -1
-			for col := 0; col < len(input[0]); col++ {
-				if pre == input[row][col] {
-					input[row][col-1] = input[row][col] + pre
-					input[row][col] = 0
-				} else if pre == 0 {
-					input[row][col], input[row][col-1] = input[row][col-1], input[row][col]
-				}
-				pre = input[row][col]
-			}
-		}
-	case RIGHT:
-		for row := 0; row < len(input); row++ {
-			var swapCellsCol []int
-			for col := len(input[0]) - 1; col >= 0; col-- {
-				if input[row][col] == 0 {
-					swapCellsCol = append(swapCellsCol, col)
-				} else {
-					if len(swapCellsCol) != 0 {
-						var swapCol int
-						swapCol, swapCellsCol = swapCellsCol[0], swapCellsCol[1:]
-						swapCellsCol = append(swapCellsCol, col)
-						input[row][swapCol], input[row][col] = input[row][col], input[row][swapCol]
-					}
-				}
-			}
-		}
-
-		for row := 0; row < len(input); row++ {
-			pre := -1
-			for col := len(input[0]) - 1; col >= 0; col-- {
-				if pre == input[row][col] {
-					input[row][col+1] = input[row][col] + pre
-					input[row][col] = 0
-				} else if pre == 0 {
-					input[row][col], input[row][col+1] = input[row][col+1], input[row][col]
-				}
-				pre = input[row][col]
 			}
 		}
 	}
+}
 
-	addRandNum(input)
+func upMerge(input [][]int) {
+	for col := 0; col < len(input[0]); col++ {
+		pre := -1
+		for row := 0; row < len(input); row++ {
+			if pre == input[row][col] {
+				input[row-1][col] = input[row][col] + pre
+				input[row][col] = 0
+			}
+			pre = input[row][col]
+		}
+	}
+}
+
+func downMove(input [][]int) {
+	for col := 0; col < len(input[0]); col++ {
+		var swapCellsRow []int
+		for row := len(input) - 1; row >= 0; row-- {
+			if input[row][col] == 0 {
+				swapCellsRow = append(swapCellsRow, row)
+			} else {
+				if len(swapCellsRow) != 0 {
+					var swapRow int
+					swapRow, swapCellsRow = swapCellsRow[0], swapCellsRow[1:]
+					swapCellsRow = append(swapCellsRow, row)
+					input[swapRow][col], input[row][col] = input[row][col], input[swapRow][col]
+				}
+			}
+		}
+	}
+}
+
+func downMerge(input [][]int) {
+	for col := 0; col < len(input[0]); col++ {
+		pre := -1
+		for row := len(input) - 1; row >= 0; row-- {
+			if pre == input[row][col] {
+				input[row+1][col] = input[row][col] + pre
+				input[row][col] = 0
+			}
+			pre = input[row][col]
+		}
+	}
+}
+
+func leftMove(input [][]int) {
+	for row := 0; row < len(input); row++ {
+		var swapCellsCol []int
+		for col := 0; col < len(input[0]); col++ {
+			if input[row][col] == 0 {
+				swapCellsCol = append(swapCellsCol, col)
+			} else {
+				if len(swapCellsCol) != 0 {
+					var swapCol int
+					swapCol, swapCellsCol = swapCellsCol[0], swapCellsCol[1:]
+					swapCellsCol = append(swapCellsCol, col)
+					input[row][swapCol], input[row][col] = input[row][col], input[row][swapCol]
+				}
+			}
+		}
+	}
+}
+
+func leftMerge(input [][]int) {
+	for row := 0; row < len(input); row++ {
+		pre := -1
+		for col := 0; col < len(input[0]); col++ {
+			if pre == input[row][col] {
+				input[row][col-1] = input[row][col] + pre
+				input[row][col] = 0
+			}
+			pre = input[row][col]
+		}
+	}
+}
+
+func rightMove(input [][]int) {
+	for row := 0; row < len(input); row++ {
+		var swapCellsCol []int
+		for col := len(input[0]) - 1; col >= 0; col-- {
+			if input[row][col] == 0 {
+				swapCellsCol = append(swapCellsCol, col)
+			} else {
+				if len(swapCellsCol) != 0 {
+					var swapCol int
+					swapCol, swapCellsCol = swapCellsCol[0], swapCellsCol[1:]
+					swapCellsCol = append(swapCellsCol, col)
+					input[row][swapCol], input[row][col] = input[row][col], input[row][swapCol]
+				}
+			}
+		}
+	}
+}
+
+func rightMerge(input [][]int) {
+	for row := 0; row < len(input); row++ {
+		pre := -1
+		for col := len(input[0]) - 1; col >= 0; col-- {
+			if pre == input[row][col] {
+				input[row][col+1] = input[row][col] + pre
+				input[row][col] = 0
+			}
+			pre = input[row][col]
+		}
+	}
 }
 
 func addRandNum(input [][]int) {
