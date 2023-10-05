d12cf4533d3056d9f2c74f0ab2d614c84dfd3822
York lin <superj80820@gmail.com>
2023-10-05 14:09:39 +0800

    update

---
+++ neetcode/0133-clone-graph.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+// time complexity: O(e + v)
+// space complexity: O(v)
+func cloneGraph(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+	return dfs(node, make(map[*Node]*Node))
+}
+
+func dfs(node *Node, oldToNew map[*Node]*Node) *Node {
+	if newNode, ok := oldToNew[node]; ok {
+		return newNode
+	}
+
+	newNode := &Node{Val: node.Val}
+	oldToNew[node] = newNode
+
+	for _, neighbor := range node.Neighbors {
+		newNode.Neighbors = append(newNode.Neighbors, dfs(neighbor, oldToNew))
+	}
+
+	return newNode
+}
